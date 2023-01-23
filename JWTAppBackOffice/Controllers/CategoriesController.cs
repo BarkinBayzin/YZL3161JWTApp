d7ea36826bb50fba11b80140e6963ce1c2c1ec35
Barkın Bayzin <bayesiian@gmail.com>
2023-01-23 11:54:15 +0300

    CategoryController Completed..

--- JWTAppBackOffice/Controllers/CategoriesController.cs
+++ JWTAppBackOffice/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using JWTAppBackOffice.Core.Features.CQRS.Queries;
+﻿using JWTAppBackOffice.Core.Features.CQRS.Commands;
+using JWTAppBackOffice.Core.Features.CQRS.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
@@ -20,6 +21,34 @@ namespace JWTAppBackOffice.Controllers
         public async Task<IActionResult> List()
         {
             return Ok(await _mediator.Send(new GetAllCategoriesQueryRequest()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategory(int id)
+        {
+            if(id < 1) return NotFound();
+
+            return Ok(await _mediator.Send(new GetCategoryQueryRequest(id)));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryCommandRequest request)
+        {
+            await _mediator.Send(request);
+            return Created("", request);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateCategoryCommandRequest request)
+        {
+            return Ok(await _mediator.Send(request));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeleteCategoryCommandRequest(id));
+            return NoContent();
         }
     }
 }
